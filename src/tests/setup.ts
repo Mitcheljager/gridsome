f91e3f10ad8544d3a214655559b0e5f623ae0240
Mitchel <mitchel_jager@hotmail.com>
2025-01-18 23:51:25 +0100

    test: Mock animations

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,18 @@
 import { cleanup } from "@testing-library/svelte"
-import { afterEach } from "vitest"
+import { afterEach, beforeAll, vi } from "vitest"
+
+const mockAnimations = (): void => {
+  Element.prototype.animate ??= vi.fn().mockReturnValue({
+    finished: Promise.resolve(),
+    cancel: vi.fn(),
+    startTime: null,
+    currentTime: null
+  })
+}
+
+beforeAll(() => {
+  mockAnimations()
+})
 
 afterEach(() => {
   cleanup()
