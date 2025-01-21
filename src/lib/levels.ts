0603260dc89bdf3984441d849ac3a5dce696dd65
Mitchel <mitchel_jager@hotmail.com>
2025-01-21 01:45:31 +0100

    feat: Add more levels

--- src/lib/levels.ts
+++ src/lib/levels.ts
@@ -25,9 +25,51 @@ export const levels: Level[] = [{
   goal: [1, 5, 5, 1],
   maxCellValue: 5
 }, {
+  id: "09mas",
+  gridSize: 2,
+  start: [1, 2, 2, 3],
+  goal: [5, 3, 3, 5],
+  maxCellValue: 5
+}, {
   id: "gojkd",
   gridSize: 2,
   start: [1, 2, 3, 4],
   goal: [6, 6, 6, 6],
   maxCellValue: 6
+}, {
+  id: "0ls53",
+  gridSize: 2,
+  start: [2, 4, 6, 8],
+  goal: [1, 1, 1, 1],
+  maxCellValue: 8
+}, {
+  id: "798eu",
+  gridSize: 2,
+  start: [2, 8, 9, 1],
+  goal: [2, 9, 5, 3],
+  maxCellValue: 9
+}, {
+  id: "bvcdy",
+  gridSize: 2,
+  start: [9, 5, 5, 9],
+  goal: [1, 4, 9, 1],
+  maxCellValue: 9
+}, {
+  id: "gyer2r",
+  gridSize: 2,
+  start: [12, 12, 12, 12],
+  goal: [7, 8, 9, 10],
+  maxCellValue: 12
+}, {
+  id: "32ntg",
+  gridSize: 2,
+  start: [6, 15, 15, 1],
+  goal: [15, 6, 6, 12],
+  maxCellValue: 15
+}, {
+  id: "a7ona",
+  gridSize: 3,
+  start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  goal:  [1, 1, 1, 1, 2, 1, 1, 1, 1],
+  maxCellValue: 2
 }]
