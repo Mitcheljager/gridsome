d3abc3e394b613786229c8bf1d0e1d20fc8ce15b
Mitchel <mitchel_jager@hotmail.com>
2025-01-21 01:45:57 +0100

    feat: Add grid size selector

--- src/routes/levels/+page.svelte
+++ src/routes/levels/+page.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
 	import { fly, scale } from "svelte/transition"
 	import { levels } from "$lib/levels"
 	import { getCompletedLevel } from "$lib/game"
 	import BackButton from "../BackButton.svelte"
+	import { browser } from "$app/environment"
+
+  let selectedGridSize = $state(2)
 </script>
 
 <div class="layout">
@@ -10,12 +13,30 @@
 
   <h1 in:fly={{ y: -(Math.min(window.innerWidth / 20, 40)), duration: 300 }}>Levels</h1>
 
+  <h2>Type</h2>
+
+  <nav class="options">
+    {#each [2, 3] as option, i}
+      <button class="tile option" class:active={option === selectedGridSize} onclick={(): void => { selectedGridSize = option }} in:scale|global={{ duration: 200, delay: 150 + i * 50, start: 0.85 }}>
+        <div class="grid" style:--grid-size={option}>
+          {#each { length: option * option } as _}
+            <div class="cell"></div>
+          {/each}
+        </div>
+
+        {option}x{option}
+      </button>
+    {/each}
+  </nav>
+
+  <h2>Level</h2>
+
   <nav class="levels">
-    {#each levels as { id }, i}
-      {@const completed = !!getCompletedLevel(id)}
+    {#each levels.filter(({ gridSize }) => selectedGridSize === gridSize) as { id }, i (id)}
+      {@const completed = browser && !!getCompletedLevel(id)}
 
       <a
-        class="level"
+        class="tile"
         class:completed
         href="/levels/{id}"
         in:scale|global={{ duration: 200, delay: 150 + i * 50, start: 0.85 }}>
@@ -33,6 +54,14 @@
     font-size: clamp(4rem, 20vw, 7rem);
   }
 
+  h2 {
+    margin: 0;
+    padding-bottom: clamp(1rem, 5vw, 2rem);
+    text-align: center;
+    font-size: clamp(2rem, 10vw, 3rem);
+    line-height: 1;
+  }
+
   .layout {
     position: relative;
     max-width: var(--max-width);
@@ -47,7 +76,7 @@
     padding: 0 0 2rem;
   }
 
-  .level {
+  .tile {
     position: relative;
     display: flex;
     align-items: center;
@@ -60,19 +89,19 @@
     text-decoration: none;
     line-height: 1;
     font-size: clamp(3rem, 13vw, 5rem);
-    transition: transform 50ms;
+    transition: transform 50ms, background-color 200ms;
   }
 
-  .level:hover,
-  .level:active {
+  .tile:hover,
+  .tile:active {
     outline: 2px solid white;
   }
 
-  .level:active {
+  .tile:active {
     transform: scale(0.9);
   }
 
-  .level::before {
+  .tile::before {
     content: "";
     display: block;
     position: absolute;
@@ -85,8 +114,8 @@
     z-index: -1;
   }
 
-  .level:hover::before,
-  .level:active::before {
+  .tile:hover::before,
+  .tile:active::before {
     border-color: transparent;
   }
 
@@ -97,5 +126,60 @@
 
   .completed::before {
     background: var(--success);
+  }
+
+  .options {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+    margin-bottom: 2rem;
+  }
+
+  .option {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: clamp(1rem, 5vw, 2rem);
+    appearance: none;
+    background: transparent;
+    border: 0;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: clamp(1.5rem, 10vw, 2.5rem);
+    line-height: 0.75;
+    color: white;
+    transition: color 200ms;
+  }
+
+  .option:active {
+    transform: scale(0.95);
+  }
+
+  .option.active {
+    color: black;
+  }
+
+  .option.active::before {
+    background: white;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(var(--grid-size), 1fr);
+    gap: calc(clamp(0.5rem, 3vw, 1rem) / var(--grid-size));
+    width: 50%;
+    aspect-ratio: 1 / 1;
+  }
+
+  .cell {
+    width: 100%;
+    aspect-ratio: 1 / 1;
+    border-radius: calc(clamp(0.25rem, 3vw, 1rem) / var(--grid-size));
+    background: white;
+    transition: background-color 200ms;
+  }
+
+  .active .cell {
+    background: black;
   }
 </style>
