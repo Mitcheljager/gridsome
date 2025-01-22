ef3ab9ed41e5751f8ecb3d5d594fa238359456d9
Mitchel <mitchel_jager@hotmail.com>
2025-01-22 23:54:56 +0100

    chore: Add capacitor splash screen with icon assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from "@capacitor/cli"
 const config: CapacitorConfig = {
   appId: "com.quitequiet.gridsome",
   appName: "gridsome",
-  webDir: "build"
+  webDir: "build",
+  plugins: {
+    SplashScreen: {
+      backgroundColor: "#203a43",
+      showSpinner: true,
+      spinnerColor: "#00ffbf",
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 }
 
 export default config
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,19 @@
   import "$lib/global.css"
 	import { setAlternativeFont } from "$lib/settings"
 	import { browser } from "$lib/utils"
+	import { Capacitor } from "@capacitor/core"
+	import { onMount } from "svelte"
 
   const { children } = $props()
 
   if (browser) setAlternativeFont()
+
+  onMount(async() => {
+    if (!Capacitor.isNativePlatform()) return
+
+    const { SplashScreen } = await import("@capacitor/splash-screen")
+    await SplashScreen.hide()
+  })
 </script>
 
 <main data-sveltekit-preload-code="viewport">
