1d3a56ea8ae2424ec61c3ab3229d924f48a67350
Mitchel <mitchel_jager@hotmail.com>
2025-01-23 22:05:16 +0100

    chore: Add star goals to each level

--- src/lib/levels.js
+++ src/lib/levels.js
@@ -3,359 +3,419 @@ export const levels = [{
   gridSize: 2,
   start: [1, 1, 1, 1],
   goal: [1, 1, 2, 2],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: [4, 2, 1]
 }, {
   id: "guag3",
   gridSize: 2,
   start: [1, 2, 2, 1],
   goal: [2, 1, 1, 2],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: [5, 3, 2]
 }, {
   id: "okzm15",
   gridSize: 2,
   start: [1, 2, 2, 1],
   goal: [3, 1, 1, 3],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [5, 3, 2]
 }, {
   id: "187bb",
   gridSize: 2,
   start: [1, 2, 1, 2],
   goal: [3, 3, 3, 1],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [7, 5, 3]
 }, {
   id: "l9076",
   gridSize: 2,
   start: [3, 2, 2, 3],
   goal: [3, 2, 2, 2],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [7, 5, 3]
 }, {
   id: "l9x98",
   gridSize: 2,
   start: [4, 3, 2, 1],
   goal: [1, 2, 3, 4],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [6, 4, 2]
 }, {
   id: "ioja1",
   gridSize: 2,
   start: [4, 2, 2, 4],
   goal: [3, 2, 3, 2],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [9, 6, 3]
 }, {
   id: "hngy2",
   gridSize: 2,
   start: [4, 2, 2, 4],
   goal: [3, 4, 3, 2],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [10, 7, 4]
 }, {
   id: "iasd6",
   gridSize: 2,
   start: [2, 2, 2, 2],
   goal: [1, 5, 5, 1],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: [9, 6, 3]
 }, {
   id: "09mas",
   gridSize: 2,
   start: [1, 2, 2, 3],
   goal: [5, 3, 3, 5],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: [8, 6, 4]
 }, {
   id: "873t4",
   gridSize: 2,
   start: [2, 3, 2, 5],
   goal: [5, 4, 3, 2],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: [10, 8, 5]
 }, {
   id: "gojkd",
   gridSize: 2,
   start: [1, 2, 3, 4],
   goal: [6, 6, 6, 6],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [12, 8, 5]
 }, {
   id: "g91al",
   gridSize: 2,
   start: [6, 2, 2, 6],
   goal: [6, 1, 2, 3],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [12, 8, 4]
 }, {
   id: "3bdi1",
   gridSize: 2,
   start: [1, 3, 6, 4],
   goal: [6, 5, 4, 3],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [12, 8, 4]
 }, {
   id: "1p798",
   gridSize: 2,
   start: [6, 3, 6, 6],
   goal: [2, 6, 3, 4],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [14, 10, 6]
 }, {
   id: "0ls53",
   gridSize: 2,
   start: [2, 4, 6, 8],
   goal: [1, 1, 1, 1],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [16, 12, 6]
 }, {
   id: "289gf",
   gridSize: 2,
   start: [1, 3, 5, 7],
   goal: [5, 5, 5, 5],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [16, 12, 6]
 }, {
   id: "izjxc",
   gridSize: 2,
   start: [5, 2, 7, 4],
   goal: [2, 4, 6, 8],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [20, 14, 7]
 }, {
   id: "798eu",
   gridSize: 2,
   start: [2, 8, 9, 1],
   goal: [2, 9, 5, 3],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [16, 12, 6]
 }, {
   id: "byq74",
   gridSize: 2,
   start: [1, 2, 2, 1],
   goal: [6, 8, 9, 7],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [18, 12, 6]
 }, {
   id: "bvcdy",
   gridSize: 2,
   start: [9, 5, 5, 9],
   goal: [1, 4, 9, 1],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [20, 14, 8]
 }, {
   id: "yunweg",
   gridSize: 2,
   start: [10, 10, 10, 10],
   goal: [1, 3, 5, 7],
-  maxCellValue: 10
+  maxCellValue: 10,
+  stars: [20, 15, 8]
 }, {
   id: "17890",
   gridSize: 2,
   start: [4, 2, 3, 1],
   goal: [7, 8, 10, 9],
-  maxCellValue: 10
+  maxCellValue: 10,
+  stars: [22, 16, 8]
 }, {
   id: "gyer2r",
   gridSize: 2,
   start: [12, 12, 12, 12],
   goal: [7, 8, 9, 10],
-  maxCellValue: 12
+  maxCellValue: 12,
+  stars: []
 }, {
   id: "pow8k",
   gridSize: 2,
   start: [11, 1, 1, 11],
   goal: [10, 12, 6, 8],
-  maxCellValue: 12
+  maxCellValue: 12,
+  stars: [20, 12, 7]
 }, {
   id: "32ntg",
   gridSize: 2,
   start: [6, 15, 15, 1],
   goal: [15, 6, 6, 12],
-  maxCellValue: 15
+  maxCellValue: 15,
+  stars: [30, 20, 13]
 }, {
   id: "v2aso",
   gridSize: 2,
   start: [9, 15, 1, 12],
   goal: [1, 1, 2, 5],
-  maxCellValue: 15
+  maxCellValue: 15,
+  stars: [35, 25, 15]
 }, {
   id: "a0911",
   gridSize: 2,
   start: [7, 6, 5, 4],
   goal: [3, 5, 7, 9],
-  maxCellValue: 15
+  maxCellValue: 15,
+  stars: [20, 15, 9]
 }, {
-  id: "ala890",
+  id: "ala89",
   gridSize: 2,
   start: [2, 15, 5, 8],
   goal: [3, 14, 14, 9],
-  maxCellValue: 15
+  maxCellValue: 15,
+  stars: [25, 20, 13]
 }, {
   id: "gmawo",
   gridSize: 2,
   start: [14, 1, 1, 14],
   goal: [3, 12, 13, 7],
-  maxCellValue: 15
+  maxCellValue: 15,
+  stars: [30, 22, 13]
 }, {
   id: "klzl0",
   gridSize: 3,
   start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
   goal:  [1, 2, 2, 1, 1, 1, 2, 2, 1],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: [10, 6, 2]
 }, {
   id: "y12ah",
   gridSize: 3,
   start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
   goal:  [2, 1, 1, 1, 1, 1, 1, 1, 2],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: [15, 8, 4]
 }, {
   id: "byy17",
   gridSize: 3,
   start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
   goal:  [2, 1, 1, 2, 2, 2, 1, 1, 2],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: [9, 6, 3]
 }, {
   id: "a7ona",
   gridSize: 3,
   start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
   goal:  [1, 1, 1, 1, 2, 1, 1, 1, 1],
-  maxCellValue: 2
+  maxCellValue: 2,
+  stars: []
 }, {
   id: "1267a",
   gridSize: 3,
   start: [1, 3, 1, 2, 1, 2, 1, 3, 1],
   goal:  [3, 3, 3, 3, 3, 3, 3, 3, 3],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [15, 10, 5]
 }, {
   id: "b7177",
   gridSize: 3,
   start: [2, 1, 1, 3, 1, 3, 1, 1, 2],
   goal:  [3, 2, 1, 3, 2, 1, 3, 2, 1],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [12, 8, 4]
 }, {
   id: "loaio",
   gridSize: 3,
   start: [2, 2, 3, 3, 1, 3, 3, 2, 2],
   goal:  [1, 1, 1, 1, 1, 1, 1, 1, 1],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [8, 6, 4]
 }, {
   id: "ze981",
   gridSize: 3,
   start: [3, 3, 3, 3, 3, 3, 3, 3, 3],
   goal:  [2, 1, 2, 1, 3, 1, 2, 1, 2],
-  maxCellValue: 3
+  maxCellValue: 3,
+  stars: [10, 8, 4]
 }, {
   id: "18gnh",
   gridSize: 3,
   start: [3, 2, 1, 1, 4, 1, 1, 2, 3],
   goal:  [3, 3, 2, 2, 1, 2, 2, 3, 3],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [20, 15, 7]
 }, {
   id: "93azz",
   gridSize: 3,
   start: [3, 3, 2, 2, 3, 2, 2, 3, 3],
   goal:  [3, 3, 2, 2, 1, 2, 2, 3, 3],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [30, 20, 10]
 }, {
   id: "65loo",
   gridSize: 3,
   start: [2, 2, 2, 1, 4, 1, 2, 2, 2],
   goal:  [1, 4, 4, 1, 1, 1, 4, 4, 1],
-  maxCellValue: 4
+  maxCellValue: 4,
+  stars: [20, 10, 5]
 }, {
   id: "67yau",
   gridSize: 3,
   start: [3, 1, 4, 1, 5, 1, 4, 1, 3],
   goal:  [1, 5, 3, 5, 5, 5, 3, 5, 1],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: [20, 10, 5]
 }, {
   id: "uajb6",
   gridSize: 3,
   start: [1, 2, 3, 1, 1, 1, 3, 2, 1],
   goal:  [5, 4, 3, 2, 1, 2, 3, 4, 5],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: [18, 12, 6]
 }, {
   id: "kla1o",
   gridSize: 3,
   start: [1, 2, 4, 1, 3, 1, 4, 2, 1],
   goal:  [1, 4, 3, 3, 3, 3, 3, 4, 1],
-  maxCellValue: 5
+  maxCellValue: 5,
+  stars: []
 }, {
   id: "poiiy",
   gridSize: 3,
   start: [1, 1, 1, 1, 1, 1, 1, 1, 1],
   goal:  [2, 5, 2, 6, 6, 6, 2, 5, 2],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [24, 16, 8]
 }, {
   id: "zooq2",
   gridSize: 3,
   start: [6, 4, 6, 2, 2, 2, 6, 4, 6],
   goal:  [1, 5, 1, 4, 1, 4, 1, 5, 1],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [20, 10, 5]
 }, {
   id: "42902",
   gridSize: 3,
   start: [5, 2, 6, 2, 2, 2, 6, 2, 5],
   goal:  [2, 6, 2, 6, 6, 6, 2, 6, 2],
-  maxCellValue: 6
+  maxCellValue: 6,
+  stars: [30, 16, 8]
 }, {
   id: "1ghj8",
   gridSize: 3,
   start: [1, 2, 2, 1, 7, 1, 2, 2, 1],
   goal:  [7, 7, 7, 7, 1, 7, 7, 7, 7],
-  maxCellValue: 7
+  maxCellValue: 7,
+  stars: [20, 10, 6]
 }, {
   id: "gn66l",
   gridSize: 3,
   start: [1, 2, 2, 1, 2, 1, 2, 2, 1],
   goal:  [7, 7, 7, 7, 1, 7, 7, 7, 7],
-  maxCellValue: 7
+  maxCellValue: 7,
+  stars: [40, 30, 14]
 }, {
   id: "71239",
   gridSize: 3,
   start: [7, 5, 7, 5, 5, 5, 7, 5, 7],
   goal:  [1, 1, 1, 1, 7, 1, 1, 1, 1],
-  maxCellValue: 7
+  maxCellValue: 7,
+  stars: [30, 20, 10]
 }, {
   id: "ooiop",
   gridSize: 3,
   start: [1, 2, 3, 4, 5, 4, 3, 2, 1],
   goal:  [7, 1, 1, 1, 7, 1, 1, 1, 7],
-  maxCellValue: 7
+  maxCellValue: 7,
+  stars: [30, 20, 10]
 }, {
   id: "z0989",
   gridSize: 3,
   start: [2, 8, 2, 8, 2, 8, 2, 8, 2],
   goal:  [8, 2, 8, 2, 8, 2, 8, 2, 8],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [40, 20, 10]
 }, {
   id: "yyhaj",
   gridSize: 3,
   start: [4, 3, 2, 1, 4, 1, 2, 3, 4],
   goal:  [4, 4, 4, 4, 4, 4, 4, 4, 4],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [40, 20, 8]
 }, {
   id: "b3jap",
   gridSize: 3,
   start: [1, 5, 1, 6, 1, 6, 1, 5, 1],
   goal:  [8, 2, 2, 8, 8, 8, 2, 2, 8],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [21, 14, 7]
 }, {
   id: "891zx",
   gridSize: 3,
   start: [8, 2, 2, 2, 8, 2, 2, 2, 8],
   goal:  [2, 6, 2, 6, 6, 6, 2, 6, 2],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [50, 30, 18]
 }, {
   id: "qtuww",
   gridSize: 3,
   start: [2, 5, 8, 6, 8, 6, 8, 5, 2],
   goal:  [1, 2, 6, 1, 8, 1, 6, 2, 1],
-  maxCellValue: 8
+  maxCellValue: 8,
+  stars: [30, 15, 8]
 }, {
   id: "zzlxx",
   gridSize: 3,
   start: [9, 8, 7, 6, 5, 4, 3, 2, 1],
   goal:  [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [40, 20, 10]
 }, {
   id: "a89us",
   gridSize: 3,
   start: [9, 2, 1, 4, 2, 4, 1, 2, 9],
   goal:  [5, 9, 5, 8, 1, 8, 5, 9, 5],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [33, 22, 11]
 }, {
   id: "7yn4g",
   gridSize: 3,
   start: [1, 2, 1, 9, 7, 9, 1, 2, 1],
   goal:  [6, 6, 7, 9, 7, 9, 7, 6, 6],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [40, 20, 10]
 }, {
   id: "l745g",
   gridSize: 3,
   start: [1, 4, 1, 3, 9, 3, 1, 4, 1],
   goal:  [4, 1, 4, 9, 3, 9, 4, 1, 4],
-  maxCellValue: 9
+  maxCellValue: 9,
+  stars: [50, 30, 13]
 }]
