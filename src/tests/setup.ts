ab6ffab811dd337eeabe6a540d3845abfe228293
Mitchel <mitchel_jager@hotmail.com>
2025-01-30 22:59:07 +0100

    test: Mock audio context

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -3,6 +3,34 @@ import { cleanup } from "@testing-library/svelte"
 import { afterEach, beforeAll, vi } from "vitest"
 
 beforeAll(() => {
+  global.AudioContext = vi.fn().mockImplementation(() => ({
+    createMediaStreamSource: vi.fn(),
+    createMediaElementSource: vi.fn(),
+    createBufferSource: vi.fn().mockImplementation(() => ({
+      connect: vi.fn(),
+      start: vi.fn(),
+      stop: vi.fn(),
+      loop: false,
+      playbackRate: {
+        value: 1
+      }
+    })),
+    createGain: vi.fn().mockImplementation(() => ( {
+      gain: { value: 1 },
+      connect: vi.fn()
+    })),
+    decodeAudioData: vi.fn().mockImplementation((buffer, successCallback = vi.fn()) => {
+      successCallback(buffer)
+    }),
+    destination: {}
+  }))
+
+  // @ts-expect-error It's just a mock
+  global.fetch = vi.fn(async() => ({
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    arrayBuffer: async() => new ArrayBuffer(8)
+  }))
+
   Element.prototype.animate ??= vi.fn().mockReturnValue({
     finished: Promise.resolve(),
     cancel: vi.fn(),
